ecd70ad0744639f465f729e6dccd67c06da575cf
Jake Nichols <yummyfunnybunny13@gmail.com>
2023-12-16 16:56:38 -0700

    fix infinite random images

--- src/random-fossil.js
+++ src/random-fossil.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 // SECTION == Query Selectors ==
 
-const nameForm = document.querySelector("#name-form");
-const nameInput = document.querySelector("#namefield");
+// const nameForm = document.querySelector("#name-form");
+// const nameInput = document.querySelector("#namefield");
 const randomFossilButton = document.querySelector("#get-random-fossil");
 const randomFossilImageDiv = document.querySelector("#random-fossil-image");
 const randomFossilName = document.querySelector("#random-fossil-name");
@@ -19,40 +19,42 @@ randomFossilButton.addEventListener("click", getRandomFossil);
 
 // SECTION == Functions ==
 
-async function submitName(evt) {
-  evt.preventDefault();
-  const username = nameInput.value;
-  console.log(username);
+// async function submitName(evt) {
+//   evt.preventDefault();
+//   const username = nameInput.value;
+//   console.log(username);
 
-  const reqBody = {
-    username: username,
-  };
+//   const reqBody = {
+//     username: username,
+//   };
 
-  console.log(reqBody);
+//   console.log(reqBody);
 
-  // axios
-  //   .post("/get-name", reqBody)
-  //   .then((response) => {
-  //     console.log("hello");
-  //     console.log(response.data);
-  //   })
-  //   .catch((error) => {
-  //     console.log(error);
-  //   });
+//   axios
+//     .post("/get-name", reqBody)
+//     .then((response) => {
+//       console.log("hello");
+//       console.log(response.data);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
 
-  // await axios.post("/get-name", reqBody);
+//   await axios.post("/get-name", reqBody);
 
-  try {
-    const result = await axios.post(`/get-name`, reqBody);
-    console.log("hello");
-    console.log(result.data);
-    // window.location.href("http://localhost:8080/top-fossils");
-  } catch (err) {
-    console.log(err);
-  }
-}
+//   try {
+//     const result = await axios.post(`/get-name`, reqBody);
+//     console.log("hello");
+//     console.log(result.data);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
 
 async function getRandomFossil() {
+  randomFossilImageDiv.innerHTML = "";
+  randomFossilName.innerText = "";
+
   try {
     const result = await axios.get("/random-fossil.json");
     console.log(result.data);
