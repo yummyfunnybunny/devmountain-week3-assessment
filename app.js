090e8a921bcf3aeec51a6701901ad28e25c45c71
Jake Nichols <yummyfunnybunny13@gmail.com>
2023-12-16 16:46:15 -0700

    complete assessment

--- app.js
+++ app.js
@@ -1,72 +1,147 @@
-import express from 'express';
-import session from 'express-session';
-import lodash from 'lodash';
-import morgan from 'morgan';
-import nunjucks from 'nunjucks';
-import ViteExpress from 'vite-express';
+// SECTION == Imports ==
+
+import express from "express";
+import session from "express-session";
+import lodash from "lodash";
+import morgan from "morgan";
+import nunjucks from "nunjucks";
+import ViteExpress from "vite-express";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+// !SECTION ==============
+
+// SECTION == Initializers ==
 
 const app = express();
-const port = '8000';
+const port = "8000";
 
-app.use(morgan('dev'));
+// !SECTION ==============
+
+// SECTION == Middle-ware ==
+// json parser
+// serve static files
+// urlencoded
+app.use(express.json());
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static('public'));
-app.use(session({ secret: 'ssshhhhh', saveUninitialized: true, resave: false }));
+app.use(express.static("public"));
+app.use(
+  session({ secret: "ssshhhhh", saveUninitialized: true, resave: false })
+);
 
-nunjucks.configure('views', {
+nunjucks.configure("views", {
   autoescape: true,
   express: app,
 });
 
+// !SECTION ==============
+
+// SECTION == Databases ==
+
 const MOST_LIKED_FOSSILS = {
   aust: {
-    img: '/img/australopith.png',
-    name: 'Australopithecus',
+    img: "/img/australopith.png",
+    name: "Australopithecus",
     num_likes: 584,
   },
   quetz: {
-    img: '/img/quetzal_torso.png',
-    name: 'Quetzal',
+    img: "/img/quetzal_torso.png",
+    name: "Quetzal",
     num_likes: 587,
   },
   steg: {
-    img: '/img/stego_skull.png',
-    name: 'Stegosaurus',
+    img: "/img/stego_skull.png",
+    name: "Stegosaurus",
     num_likes: 598,
   },
   trex: {
-    img: '/img/trex_skull.png',
-    name: 'Tyrannosaurus Rex',
+    img: "/img/trex_skull.png",
+    name: "Tyrannosaurus Rex",
     num_likes: 601,
   },
 };
 
 const OTHER_FOSSILS = [
   {
-    img: '/img/ammonite.png',
-    name: 'Ammonite',
+    img: "/img/ammonite.png",
+    name: "Ammonite",
   },
   {
-    img: '/img/mammoth_skull.png',
-    name: 'Mammoth',
+    img: "/img/mammoth_skull.png",
+    name: "Mammoth",
   },
   {
-    img: '/img/ophthalmo_skull.png',
-    name: 'Opthalmosaurus',
+    img: "/img/ophthalmo_skull.png",
+    name: "Opthalmosaurus",
   },
   {
-    img: '/img/tricera_skull.png',
-    name: 'Triceratops',
+    img: "/img/tricera_skull.png",
+    name: "Triceratops",
   },
 ];
 
-// TODO: Replace this comment with your code
+// !SECTION ==============
 
-app.get('/random-fossil.json', (req, res) => {
+// SECTION == Routes ==
+
+app.get("/", (req, res) => {
+  res.render("homepage.html.njk");
+});
+
+app.post("/get-name", (req, res) => {
+  console.log("at get-name page");
+  console.log(req.body.name);
+
+  const username = req.body.name;
+  console.log(username);
+
+  req.session.username = username;
+
+  res.redirect("/top-fossils");
+});
+
+app.get("/random-fossil.json", (req, res) => {
   const randomFossil = lodash.sample(OTHER_FOSSILS);
   res.json(randomFossil);
 });
 
+app.get("/top-fossils", (req, res) => {
+  console.log("top-fossils route");
+  console.log(req.session.username);
+  if (req.session.username) {
+    console.log("there is a session username");
+    res.status(200).render("top-fossils.html.njk", {
+      fossils: MOST_LIKED_FOSSILS,
+      username: req.session.username,
+    });
+  } else {
+    console.log("there is no session username");
+    res.redirect("/");
+  }
+});
+
+app.post("/like-fossil", (req, res) => {
+  console.log("===== like-fossil route =======");
+  const likedFossil = req.body.fossilId;
+  console.log(likedFossil);
+
+  console.log(MOST_LIKED_FOSSILS[likedFossil]);
+  console.log(MOST_LIKED_FOSSILS[likedFossil].num_likes);
+  MOST_LIKED_FOSSILS[likedFossil].num_likes++;
+  console.log(MOST_LIKED_FOSSILS[likedFossil].num_likes);
+
+  res.render("thank-you.html.njk", {
+    username: req.session.username,
+  });
+});
+
+// !SECTION ==============
+
+// SECTION == Server Listener ==
+
 ViteExpress.listen(app, port, () => {
   console.log(`Server running on http://localhost:${port}...`);
 });
+
+// !SECTION ==============
--- src/random-fossil.js
+++ src/random-fossil.js
@@ -1,1 +1,68 @@
-console.log('Hello World');
+import axios from "axios";
+
+// SECTION == Query Selectors ==
+
+const nameForm = document.querySelector("#name-form");
+const nameInput = document.querySelector("#namefield");
+const randomFossilButton = document.querySelector("#get-random-fossil");
+const randomFossilImageDiv = document.querySelector("#random-fossil-image");
+const randomFossilName = document.querySelector("#random-fossil-name");
+
+// !SECTION ==============
+
+// SECTION == Event Listeners ==
+
+// nameForm.addEventListener("submit", submitName);
+randomFossilButton.addEventListener("click", getRandomFossil);
+
+// !SECTION ==============
+
+// SECTION == Functions ==
+
+async function submitName(evt) {
+  evt.preventDefault();
+  const username = nameInput.value;
+  console.log(username);
+
+  const reqBody = {
+    username: username,
+  };
+
+  console.log(reqBody);
+
+  // axios
+  //   .post("/get-name", reqBody)
+  //   .then((response) => {
+  //     console.log("hello");
+  //     console.log(response.data);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
+
+  // await axios.post("/get-name", reqBody);
+
+  try {
+    const result = await axios.post(`/get-name`, reqBody);
+    console.log("hello");
+    console.log(result.data);
+    // window.location.href("http://localhost:8080/top-fossils");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function getRandomFossil() {
+  try {
+    const result = await axios.get("/random-fossil.json");
+    console.log(result.data);
+    const randomFossilImage = document.createElement("img");
+    randomFossilImage.src = result.data.img;
+    randomFossilImageDiv.appendChild(randomFossilImage);
+    randomFossilName.innerText = result.data.name;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// !SECTION ==============
